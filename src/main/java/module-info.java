d6161c128dec57478b9fd07cdbdce4d27229c6ba
Emily Bjork <EmilyBjoerk@users.noreply.github.com>
2019-12-07 14:03:31 +0100

    Transfering WIP to other machine

---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module org.lisoft.lsml {
+    requires javafx.controls;
+    requires javafx.fxml;
+    
+    requires javax.inject;
+    
+    requires java.desktop;
+
+    exports org.lisoft.lsml.view_fx;
+}
